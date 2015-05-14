9db131f16a628999a4dbf3233e85c5ce85c451e9
Andy Reyes <andy@andyjreyes.com>
2015-05-14 06:39:47 -0700

    Added controller class files as well as their temporary storyboard representations. The reason the Storyboard looks crazy is because I was experimenting with segues that deallocate the previous view. I will have to create my own segue to do this. Also changed LICENSE so that hopefully GitHub can read it. It seems to like "LICENSE" not "LICENSE.txt"

--- Drawtastic/DescribingViewController.swift
+++ Drawtastic/DescribingViewController.swift
@@ -1,28 +1,27 @@
 //
-//  ViewController.swift
+//  DescribingViewController.swift
 //  Drawtastic
 //
-//  Created by Andy Reyes on 5/11/15.
+//  Created by Andy Reyes on 5/14/15.
 //  Copyright (c) 2015 Andy Reyes. All rights reserved.
 //
 
 import UIKit
 
-class DrawingViewController: UIViewController {
+class DescribingViewController: UIViewController {
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.hidesBackButton = true;
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
